2320ccf84e1f0faf2b69bdc19a3dcad846c9c8a7
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-05-21 12:41:01 -0400

    stage 2

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 def main():
-    board = [["X", "O", "X"], ["O", "X", "O"], ["X", "X", "O"]]  # 3X3 tic-tac-to board
-    print_board(board)
+    symbol = input()  # start boar with moves: X nand O
+    print_board(symbol)
 
-def print_board(board: list):
+
+def print_board(setup: str):
+    s_setup =list()
+    for s in setup:
+        s_setup.append(s)
+
+    board = list()
+    board.append(s_setup[:3])
+    board.append(s_setup[3:6])
+    board.append(s_setup[6:10])
+
+    print("--------")
     for i in range(len(board)):
+        print("|", end=" ")
         for pos in board[i]:
-            print(pos, " ", end="")
-        print()
+            print(pos, "", end="")
+        print("|")
+    print("--------")
+
 
 
 if __name__ == '__main__':
