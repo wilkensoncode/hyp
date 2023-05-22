1007d4ae985b9475bb2c0a5744ff5cd7ba770451
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-05-22 18:05:35 -0400

    tictacto complete

--- main.py
+++ main.py
@@ -1,92 +1,81 @@
 def main():
-    board = process_input('_XXOO_OX_')
+    instructions = """
+        Game start with empty board
+        X is the first player
+        to start playing enter a coordinate eg. 4 1
+        1 1 being the left upper corner of the board
+        3 3 being the bottom right corner of the board 
+        """
+    print(instructions)
+    board = [[' ' for _ in range(3)] for _ in range(3)]
     print_board(board)
     play_game(board)
 
-    # if is_full(board) and not is_winner(board):
-    #     print("Draw")
-    # elif is_winner(board) and is_winner(board):
-    #     print(f"{is_winner(board)} wins")
-    # elif not is_full(board) and not is_winner(board) and not is_impossible(board):
-    #     print("Game not finished")
-    # elif is_impossible(board) or is_impossible(board) and is_winner(board):
-    #     print("Impossible")
+
+def state(board):
+    if is_winner(board):
+        print(f"{is_winner(board)} wins")
+        pass
+    elif is_full(board):
+        print("Draw")
 
 
 def play_game(board: list):
-    msg_taken = "This cell is occupied! Choose another one!"
-    msg_wrong_input = "You should enter numbers!"
-    coord_msg = "Coordinates should be from 1 to 3!"
+    cell_taken = "This cell is occupied! Choose another one!"
+    wrong_input = "You should enter numbers!"
+    wrong_position = "Coordinates should be from 1 to 3!"
+
+    current_player = 'X'
 
     while True:
         pos_row, pos_col = input().split()
         if not pos_col.isnumeric() and not pos_row.isnumeric():
-            print(msg_wrong_input)
+            print(wrong_input)
         else:
             pos_row = int(pos_row)
             pos_col = int(pos_col)
+
             if pos_row > 3 or pos_col > 3:
-                print(coord_msg)
-            elif board[pos_row - 1][pos_col - 1] == "_":
-                board[pos_row - 1][pos_col - 1] = 'X'
+                print(wrong_position)
+            elif board[pos_row - 1][pos_col - 1] == " ":
+                board[pos_row - 1][pos_col - 1] = current_player
                 print_board(board)
-                break
+                if is_winner(board):
+                    state(board)
+                    break
+                elif is_full(board):
+                    state(board)
+                    break
+
             else:
-                print(msg_taken)
+                print(cell_taken)
+            current_player = 'O' if current_player == 'X' else 'X'  # switch player
 
 
 def is_full(board: list):
     for row in board:
-        if "_" in row:
+        if " " in row:
             return False
     return True
-
-
-def is_impossible(board: list):
-    count_x = sum(row.count("X") for row in board)
-    count_O = sum(row.count("O") for row in board)
-    diff = abs(count_O - count_x)
-    if diff >= 2:
-        return True
-
-    for col in range(len(board)):
-        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != '_':
-            return True
-
-    return False
-
-
-def incomplete_game(board: list):
-    count_x = sum(row.count("X") for row in board)
-    count_O = sum(row.count("O") for row in board)
-    diff = abs(count_O - count_x)
-
-    wins_x = is_winner(board) == "X"
-    wins_O = is_winner(board) == "O"
-
-    if wins_x == wins_O or diff <= 1:
-        return True
-    return False
 
 
 def is_winner(board: list):
     winners = list()
     for row in board:  # check row winner
-        if row[0] == row[1] == row[2] and row[0] != "_":
+        if row[0] == row[1] == row[2] and row[0] != " ":
             winners.append(row[0])
 
     for col in range(len(board)):  # check columns winner
-        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != "_":
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != " ":
             winners.append(board[0][col])
 
     # check diagonal winner
-    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != "_":
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
         winners.append((board[0][0]))
-    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != "_":
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != " ":
         winners.append(board[0][2])
 
     winner = len(winners)
-
     if winner == 1:
         return winners[0]
     elif winner > 1:
@@ -102,19 +91,6 @@ def print_board(board: list):
             print(pos, "", end="")
         print("|")
     print("---------")
-
-
-def process_input(string: str):  # @param str len must be 9 and choices are X O _
-    board_setup = list()
-    for s in string:
-        board_setup.append(s)
-
-    board = list()
-
-    board.append(board_setup[:3])
-    board.append(board_setup[3:6])
-    board.append(board_setup[6:10])
-    return board
 
 
 if __name__ == '__main__':
