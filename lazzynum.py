8b71caeedd96e85124aaaf7bc1ccc17616ea5009
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-05-24 07:41:16 -0400

    stage 4

--- lazzynum.py
+++ lazzynum.py
@@ -1,92 +1,120 @@
-memory = 0
+# Messages
+msg_0 = "Enter an equation"
+msg_1 = "Do you even know what numbers are? Stay focused!"
+msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
+msg_6 = " ... lazy"
+msg_7 = " ... very lazy"
+msg_8 = " ... very, very lazy"
+msg_9 = "You are"
 
-
-def check(v1, v2, v3):
-    msg_6 = " ... lazy"
-
-    msg_7 = " ... very lazy"
-
-    msg_8 = " ... very, very lazy"
-
-    msg_9 = "You are"
-
-    msg = ""
-    if is_one_digit(v1) and is_one_digit(v2):
-        msg = msg + msg_6
-    if v1 == 1 or v2 == 1 and v3 == '*':
-        msg = msg + msg_7
-    if v1 == 0 or v2 == 0 and v3 == "-":
-        msg = msg + msg_8
-    if msg != "":
-        msg = msg_9 + msg
-    print(msg)
+# Stores previous calculations
+memory = 0.0
 
 
 def is_one_digit(n):
     output = False
-    if -10 < n < 10 and n.is_digit():
+    if n.is_integer() and (-10 < n < 10):
         output = True
     return output
 
 
-class Calc:
-    msg_0 = "Enter an equation"
-    msg_1 = "Do you even know what numbers are? Stay focused!"
-    msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
-    msg_3 = "Yeah... division by zero. Smart move..."
-    msg_4 = "Do you want to store the result? (y / n): \n"
-    msg_5 = "Do you want to continue calculations? (y / n): \n"
+def check(v1, v2, v3):
+    msg = ""
 
-    def calc(x, oper, y):
-        if oper == '+':
-            return x + y
-        elif oper == '-':
-            return x - y
-        elif oper == '*':
-            return x * y
-        elif oper == '/':
-            return x / y
+    if is_one_digit(v1) and is_one_digit(v2):
+        msg += msg_6
+    if (v1 == 1 or v2 == 1) and (v3 == '*'):
+        msg += msg_7
+    if (v1 == 0 or v2 == 0) and (v3 != '/'):
+        msg += msg_8
+    if msg != '':
+        msg = msg_9 + msg
 
-    def not_number(x, y):
-        if x.isalpha() and y.isalpha():
-            return True
-        else:
-            return Calc.msg_1
-
-    def is_oper(self, operator):
-        oper = ['+', '-', '*', '/']
-        if operator in oper:
-            self.check(x, y, oper)
-        else:
-            print(Calc.msg_2)
+    print(msg)
 
 
-while True:
+# Performs every kind of validations
+def validate(x, oper, y):
+    result = False
 
     try:
-        result = 0
-        print(Calc.msg_0)
-        x, oper, y = input().split()
-        if x.isalpha():
-            x = memory
-        if y.isalpha():
-            y = memory
-        if Calc.is_oper(oper) and Calc.not_number(x, y):
-            result = Calc.calc(float(x), oper, float(y))
-            # check(x,y,oper)
-        is_yes = input(Calc.msg_4).lower()
-        if is_yes == 'y':
-            memory = result
-        else:
-            pass
-        is_continue = input(Calc.msg_5).lower()
-        if is_continue == 'y':
-            pass
-        else:
-            break
-    except ZeroDivisionError:
-        print(Calc.msg_3)
+        # Tests if the numbers are valid
+        x = float(x)
+        y = float(y)
     except ValueError:
-        print(Calc.msg_1)
+        print(msg_1)
+    else:
+        # Tests if the operator is valid
+        if oper not in ['+', '-', '*', '/']:
+            print(msg_2)
+        else:
+            # Tests for laziness
+            check(x, y, oper)
 
-check()
+            # Tests for division by zero
+            if oper == '/' and y == 0:
+                print(msg_3)
+            else:
+                result = True
+
+    return result
+
+
+# Do the math
+def calculate(x, oper, y):
+    result = 0.0
+
+    if oper == '+':
+        result = x + y
+    elif oper == '-':
+        result = x - y
+    elif oper == '*':
+        result = x * y
+    elif oper == '/':
+        result = x / y
+    print(result)
+
+    return result
+
+
+# Calculates until the user say 'n'...
+while True:
+    # Asks for an equation
+    print(msg_0)
+    calc = input()
+    x, oper, y = calc.split()
+
+    # Tests if any operand uses the memory
+    if x == 'M':
+        x = memory
+    if y == 'M':
+        y = memory
+
+    # Validates the equation informed
+    if not validate(x, oper, y):
+        continue
+
+    # Does the calculation
+    result = calculate(float(x), oper, float(y))
+
+    # Repeat until the answer is 'y' or 'n'
+    answer = ''
+    while answer not in ['y', 'n']:
+        # Asks if want to store the result
+        print(msg_4)
+        answer = input()
+        if answer == 'y':
+            memory = result
+
+    # Repeat until the answer is 'y' or 'n'
+    answer = ''
+    while answer not in ['y', 'n']:
+        # Asks if want to calculate again
+        print(msg_5)
+        answer = input()
+
+    if answer == 'n':
+        break
