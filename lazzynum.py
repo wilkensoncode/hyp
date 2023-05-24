de8b6d10b386b9f18348ae7b08c3d1db5c491ccc
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-05-24 16:42:18 -0400

    stage complete

--- lazzynum.py
+++ lazzynum.py
@@ -1,120 +1,82 @@
-# Messages
-msg_0 = "Enter an equation"
-msg_1 = "Do you even know what numbers are? Stay focused!"
-msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
-msg_3 = "Yeah... division by zero. Smart move..."
-msg_4 = "Do you want to store the result? (y / n):"
-msg_5 = "Do you want to continue calculations? (y / n):"
-msg_6 = " ... lazy"
-msg_7 = " ... very lazy"
-msg_8 = " ... very, very lazy"
-msg_9 = "You are"
+MSG_0 = "Enter an equation"
+MSG_1 = "Do you even know what numbers are? Stay focused!"
+MSG_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+MSG_3 = "Yeah... division by zero. Smart move..."
+MSG_4 = "Do you want to store the result? (y / n):\n"
+MSG_5 = "Do you want to continue calculations? (y / n):\n"
+MSG_6 = " ... lazy"
+MSG_7 = " ... very lazy"
+MSG_8 = " ... very, very lazy"
+MSG_9 = "You are"
+MSG_10 = "Are you sure? It is only one digit! (y / n) 10\n"
+MSG_11 = "Don't be silly! It's just one number! Add to the memory? (y / n) 11\n"
+MSG_12 = "Last chance! Do you really want to embarrass yourself? (y / n) 12\n"
 
-# Stores previous calculations
-memory = 0.0
 
-
-def is_one_digit(n):
-    output = False
-    if n.is_integer() and (-10 < n < 10):
-        output = True
-    return output
+def is_one_digit(v):
+    if -10 < v < 10 and v == int(v):
+        return True
+    else:
+        return False
 
 
 def check(v1, v2, v3):
     msg = ""
-
     if is_one_digit(v1) and is_one_digit(v2):
-        msg += msg_6
-    if (v1 == 1 or v2 == 1) and (v3 == '*'):
-        msg += msg_7
-    if (v1 == 0 or v2 == 0) and (v3 != '/'):
-        msg += msg_8
+        msg += MSG_6
+    if v1 == 1 or v2 == 1:
+        msg += MSG_7
+    if (v1 == 0 or v2 == 0) and v3 != "/":
+        msg += MSG_8
     if msg != '':
-        msg = msg_9 + msg
-
-    print(msg)
-
-
-# Performs every kind of validations
-def validate(x, oper, y):
-    result = False
-
-    try:
-        # Tests if the numbers are valid
-        x = float(x)
-        y = float(y)
-    except ValueError:
-        print(msg_1)
-    else:
-        # Tests if the operator is valid
-        if oper not in ['+', '-', '*', '/']:
-            print(msg_2)
-        else:
-            # Tests for laziness
-            check(x, y, oper)
-
-            # Tests for division by zero
-            if oper == '/' and y == 0:
-                print(msg_3)
-            else:
-                result = True
-
-    return result
+        msg = MSG_9 + msg
+        print(msg)
 
 
-# Do the math
-def calculate(x, oper, y):
-    result = 0.0
+operations = {
+    "+": (lambda x_num, y_num: x_num + y),
+    "-": (lambda x_num, y_num: x_num - y),
+    "*": (lambda x_num, y_num: x_num * y),
+    "/": (lambda xx_num, y_num: xx_num / y),
+}
 
-    if oper == '+':
-        result = x + y
-    elif oper == '-':
-        result = x - y
-    elif oper == '*':
-        result = x * y
-    elif oper == '/':
-        result = x / y
-    print(result)
+memory = 0.0
 
-    return result
-
-
-# Calculates until the user say 'n'...
 while True:
-    # Asks for an equation
-    print(msg_0)
-    calc = input()
-    x, oper, y = calc.split()
+    print(MSG_0)
+    x, oper, y = input().split()
+    try:
+        x = memory if x == "M" else float(x)
+        y = memory if y == "M" else float(y)
+        check(x, y, oper)
+        result = operations[oper](x, y)
+        print(result)
+        if input(MSG_4) == "y":  # yes store result
+            if memory < result:
+                memory = result
 
-    # Tests if any operand uses the memory
-    if x == 'M':
-        x = memory
-    if y == 'M':
-        y = memory
+            if is_one_digit(result):
+                msg_index = 10
+                if input(MSG_10) == 'y':
+                    # memory = result
+                    # print(memory)
+                    if input(MSG_11) == 'y':
+                        if msg_index < 12:
+                            msg_index = msg_index + 1
+                        else:
+                            memory = result
+                        if input(MSG_12) == 'y':
+                            memory = result
 
-    # Validates the equation informed
-    if not validate(x, oper, y):
-        continue
+                            # else:
+            #  memory = result
 
-    # Does the calculation
-    result = calculate(float(x), oper, float(y))
+        if input(MSG_5) == "n":
+            break
 
-    # Repeat until the answer is 'y' or 'n'
-    answer = ''
-    while answer not in ['y', 'n']:
-        # Asks if want to store the result
-        print(msg_4)
-        answer = input()
-        if answer == 'y':
-            memory = result
-
-    # Repeat until the answer is 'y' or 'n'
-    answer = ''
-    while answer not in ['y', 'n']:
-        # Asks if want to calculate again
-        print(msg_5)
-        answer = input()
-
-    if answer == 'n':
-        break
+    except ValueError:
+        print(MSG_1)
+    except KeyError:
+        print(MSG_2)
+    except ZeroDivisionError:
+        print(MSG_3)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tictacto import tictacto
 
 def main():
 # tictacto()
-    lazzynum.p()
+    lazzynum
 
 if __name__ == '__main__':
     main()
