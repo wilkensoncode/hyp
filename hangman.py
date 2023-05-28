c2907af7ccfc77707ad5238e8f9f5eeec1c6261e
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-05-28 06:48:32 -0400

    stage 4 complete

--- hangman.py
+++ hangman.py
@@ -1,37 +1,46 @@
 import random
 
 
-def hangman_guess():
+def hangman():
     word_choice = ['python', 'java', 'swift', 'javascript']
     choice = random.choice(word_choice)
-    attempt_guess(choice)
+    dont_die(choice)
 
 
-def attempt_guess(word: str):
+def dont_die(word: str):
     attempts = 8
     print(f"H A N G M A N")
     res = ['-'] * len(word)  # initialize array with dashes
+    guessed = []
     print(''.join(res))
 
     guess = input(f"Input a letter: ")
     while attempts > 0:
-        if guess in word:
+        if guess in word and guess not in guessed:
             for i in range(len(word)):
                 if word[i] == guess:
                     res[i] = guess
-        else:
-            print(f"That letter doesn't appear in the word")
+                    guessed.append(guess)
 
-        attempts -= 1
+        else:
+            if guess in guessed:
+                attempts -= 1
+                print(f"No improvements.")
+            else:
+                attempts -= 1
+                print(f"That letter doesn't appear in the word")
+
         if '-' not in res:
             print(''.join(res))
-            print("Thanks for playing!")
+            print("You guessed the word!")
+            print("You survived!")
             break
+
         print(''.join(res))
         if attempts == 0:
-            print("Thanks for playing!")
+            print("You lost!\nThanks for playing!")
             break
         guess = input(f"Input a letter: ")
 
 
-hangman_guess()
+hangman()
