446336ba1af1d887c8fd23da2f077539656f26ab
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-06-01 19:59:57 -0400

    markdown f stage complete

--- river.py
+++ river.py
@@ -1,13 +1,18 @@
- 
-class River:
-    # list of all rivers
-    all_rivers = []
+import math
 
-    def __init__(self, name, length):
-        self.name = name
-        self.length = length
-        # add current river to the list of all rivers
-        River.all_rivers.append(self)
+c = filter(lambda x: x % 2, [0, 1, 2, 3, 4,53])
+print(list(c))
+
+
+class River:
+	# list of all rivers
+	all_rivers = []
+
+	def __init__(self, name, length):
+		self.name = name
+		self.length = length
+		# add current river to the list of all rivers
+		River.all_rivers.append(self)
 
 
 volga = River("Volga", 3530)
@@ -16,15 +21,15 @@ nile = River("Nile", 6852)
 
 # print all river names
 for river in River.all_rivers:
-    print(river.length)
+	print(river.length)
 
 
 class Person:
-    def __init__(self, name):
-        self.name = name
+	def __init__(self, name):
+		self.name = name
 
-    def greet(self):
-        return f"Hello, I am {self.name}!"
+	def greet(self):
+		return f"Hello, I am {self.name}!"
 
 
 person = Person(input())
@@ -32,10 +37,10 @@ print(person.greet())
 
 
 def calculate(amount, interest_rate, time):
-    interest = amount * interest_rate * time / 100
-    total_amount = amount + interest
-    return interest, total_amount
+	interest = amount * interest_rate * time / 100
+	total_amount = amount + interest
+	return interest, total_amount
 
 
 def print_result(interest, total_amount):
-    print(f"The interest is: {interest}\nThe total amount is: {total_amount}")
+	print(f"The interest is: {interest}\nThe total amount is: {total_amount}")
