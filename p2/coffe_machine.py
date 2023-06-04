c75e0a19881c1b718fc49869022fa7a5a0400a24
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-06-04 16:28:44 -0400

    coffee amchine

--- p2/coffe_machine.py
+++ p2/coffe_machine.py
@@ -1,6 +1,6 @@
 def main():
     make_coffe = CoffeMachine()
-
+    """
     # print ingredient needed for x cup of coffe
 
     num_cup_coffe = int(input("Write how many cups of coffee you will need:\n "))
@@ -20,13 +20,55 @@ def main():
     cups = int(input("Write how many cups of coffee you will need:\n"))
     answer = make_coffe.inventory(water, milk, beans, cups)
     print(answer)
+    
+    """
+
+    # buy fill take tasks to perform
+
+    action = input("Write action (buy, fill, take, remaining, exit):\n")
+    while action != "exit":
+        if action == "buy":
+            choice = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n")
+            if choice.isdigit():
+                print(make_coffe.perform_action(int(choice)))
+        elif action == "fill":
+            amt_water = int(input("Write how many ml of water you want to add:\n"))
+            amt_milk = int(input("Write how many ml of milk you want to add:\n"))
+            amt_beans = int(input("Write how many grams of coffee beans you want to add:\n"))
+            amt_cups = int(input("Write how many disposable cups you want to add:\n"))
+            make_coffe.fill(water=amt_water, cups=amt_cups, beans=amt_beans, milk=amt_milk)
+        elif action == "remaining":
+            print(make_coffe.machine_state())
+        elif action == "take":
+            withdraw = make_coffe.take()
+            print(f"I gave you ${withdraw}")
+        action = input("Write action (buy, fill, take,  remaining, exit):\n")
 
 
-class CoffeMachine:
+class CoffeMachine(object):
     def __init__(self):
         self.water = 200
         self.milk = 50
         self.beans = 15
+
+        #  coffe current ingredient
+        self.money_c = 550
+        self.water_c = 400
+        self.milk_c = 540
+        self.beans_c = 120
+        self.cups = 9
+
+        self.msg = "I have enough resources, making you a coffee!"
+
+    def machine_state(self):
+        state = f"The coffee machine has:\n" \
+                f"{self.water_c} ml of water\n" \
+                f"{self.milk_c} ml of milk\n" \
+                f"{self.beans_c} g of coffee beans\n" \
+                f"{self.cups} disposable cups\n" \
+                f"${self.money_c} of money"
+
+        return state
 
     def ingredient(self, num_cups):  # ingredient needed to make 25 cups coffee
         water = self.water * num_cups
@@ -47,6 +89,61 @@ class CoffeMachine:
             ms = f"Yes, I can make that amount of coffee (and even {max_c - cup} more than that)"
 
         return ms
+
+    # sell coffee
+    def perform_action(self, action: int):
+        if action == 1:
+            return self.expresso()
+        elif action == 2:
+            return self.latte()
+        elif action == 3:
+            return self.cappuccino()
+
+    def expresso(self, water=250, beans=16, cost=4):  # helper perform_action
+        if self.water_c >= water and self.beans_c >= beans:
+            self.update_ingredient(beans, cost, water)
+            return self.msg
+        return self.feedback(beans=beans, milk=None, water=water)
+
+    def update_ingredient(self, beans, cost, water):  # helper
+        if self.cups < 1:
+            return "Sorry not enough cups"
+        self.water_c -= water
+        self.beans_c -= beans
+        self.cups -= 1
+        self.money_c += cost
+
+    def latte(self, water=350, milk=75, beans=20, cost=7):  # helper perform_action
+        if self.water_c >= water and self.milk_c >= milk and self.beans_c >= beans:
+            self.milk_c -= milk
+            self.update_ingredient(beans, cost, water)
+            return self.msg
+        return self.feedback(beans, milk, water)
+
+    def feedback(self, beans, milk, water):  # helper
+        if self.water_c < water:
+            return "Sorry, not enough water!"
+        elif self.milk < milk:
+            return "Sorry, not enough milk"
+        elif self.beans < beans:
+            return "Sorry, not enough beans"
+
+    def cappuccino(self, water=200, milk=100, beans=12, cost=6):  # helper perform_action
+        if self.water_c >= water and self.milk_c >= milk and self.beans_c >= beans:
+            self.milk_c -= milk
+            self.update_ingredient(beans, cost, water)
+            return self.msg
+        return self.feedback(beans, milk, water)
+
+    def fill(self, water, beans, milk, cups):
+        self.milk_c += milk
+        self.water_c += water
+        self.beans_c += beans
+        self.cups += cups
+
+    def take(self):
+        self.money_c -= self.money_c
+        return self.money_c
 
 
 main()
