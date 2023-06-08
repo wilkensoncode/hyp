73df70a89b81a9187046e30b9a1c7fbfaf12eff9
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-06-07 22:06:06 -0400

    main

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-#
-#
-# def main():
-#     # print(os.getcwd())
-#
-#
-# if __name__ == '__main__':
-#     main()
+import re
+
+regexp = '..ever gonna'
+string = '\\'
+result = re.match(r"\\", string)
+print(result)
