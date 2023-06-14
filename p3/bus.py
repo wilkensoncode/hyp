89e33f49708cdd1e68dc525b2d846144469ef6b9
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-06-14 19:25:33 -0400

    wrongt time

--- p3/bus.py
+++ p3/bus.py
@@ -1,37 +1,57 @@
-from re import fullmatch
 import json
 
 
 def bus_info():
-	validation = {
-		'bus_id': {'type': int, 're_pattern': r"\d+"},
-		'stop_id': {'type': int, 're_pattern': r"\d+"},
-		'stop_name': {'type': str, 're_pattern': r"(?:[A-Z][A-Za-z]?[a-z]+ ?)+(Avenue|Street|Road|Boulevard)$"},
-		'next_stop': {'type': int, 're_pattern': r"\d+"},
-		'stop_type': {'type': str, 're_pattern': r"^(|[SOF])$"},
-		'a_time': {'type': str, 're_pattern': r"^(?:[01][0-9]|2[0-3]):[0-5][0-9]$"}
-	}
+	data = json.loads(input())
 
-	err_lst = dict.fromkeys(validation, 0)
-	roads = json.loads(input())
+	entries_by_bus = {}
+	for d in data:
+		entries_by_bus.setdefault(d["bus_id"], [])
+		entries_by_bus[d["bus_id"]].append(d)
 
-	bus_id = {}  # map bus id to # stops
+	for bus, entries in entries_by_bus.items():
+		start_stops, end_stops = 0, 0
+		for e in entries:
+			if e["stop_type"] == "S":
+				start_stops += 1
+			elif e["stop_type"] == "F":
+				end_stops += 1
+		if start_stops != 1 or end_stops != 1:
+			return print(f"There is no start or end stop for the line: {bus}.")
 
-	for route in roads:
-		for key, val in route.items():
-			if not isinstance(val, validation[key]['type']) \
-					or fullmatch(validation[key]['re_pattern'], str(val)) is None:
-				err_lst[key] += 1
-			if isinstance(val, int) and key == 'bus_id':
-				bus_id[val] = bus_id.get(val, 0) + 1
+	all_stops, start_stops, transfer_stops, finish_stops = [], [], [], []
+	for d in data:
+		all_stops.append(d["stop_name"])
+		if d["stop_type"] == "S":
+			start_stops.append(d["stop_name"])
+		elif d["stop_type"] == "F":
+			finish_stops.append(d["stop_name"])
 
-	# print(f"Format validation: {sum(err_lst.values())} errors")
-	# for k, v in err_lst.items():
-	# 	if k == 'stop_name' or k == 'stop_type' or k == 'a_time':
-	# 		print(f"{k}: {v}")
-	print("Line names and number of stops:")
-	for bus in bus_id:
-		print(f"bus_id: {bus}, stops: {bus_id[bus]}")
+	for name in set(all_stops):
+		if all_stops.count(name) > 1:
+			transfer_stops.append(name)
+
+	transfer_stops.sort()
+	arrive_time(entries_by_bus, transfer_stops, start_stops, finish_stops)
+
+
+def arrive_time(entries_by_bus, transfer_stops, start_stops, finish_stops):
+	print("Arrival time test:")
+	incorrect_arrivals = []
+	for bus, entries in entries_by_bus.items():
+		prev_time = None
+		for i in range(len(entries)):
+			curr_time = entries[i]["a_time"]
+			if prev_time and curr_time <= prev_time:
+				incorrect_arrivals.append(f"bus_id line {bus}: wrong time on station {entries[i]['stop_name']}")
+				break
+			prev_time = curr_time
+
+	if len(incorrect_arrivals) == 0:
+		print("OK")
+	else:
+		for arrival in incorrect_arrivals:
+			print(arrival)
 
 
 bus_info()
