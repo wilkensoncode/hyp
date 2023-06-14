058a71e337fe0b11790209d5287273799b2dd90c
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-06-13 23:01:10 -0400

    chn]=ange to bus

--- p3/bus.py
+++ p3/bus.py
@@ -14,6 +14,7 @@ def bus_info():
 
 	err_lst = dict.fromkeys(validation, 0)
 	roads = json.loads(input())
+
 	bus_id = {}  # map bus id to # stops
 
 	for route in roads:
@@ -24,10 +25,13 @@ def bus_info():
 			if isinstance(val, int) and key == 'bus_id':
 				bus_id[val] = bus_id.get(val, 0) + 1
 
-			# print(f"Format validation: {sum(err_lst.values())} errors")
+	# print(f"Format validation: {sum(err_lst.values())} errors")
 	# for k, v in err_lst.items():
 	# 	if k == 'stop_name' or k == 'stop_type' or k == 'a_time':
 	# 		print(f"{k}: {v}")
 	print("Line names and number of stops:")
 	for bus in bus_id:
 		print(f"bus_id: {bus}, stops: {bus_id[bus]}")
+
+
+bus_info()
