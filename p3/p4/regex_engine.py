b7c4b20b2552c43446c9c4bc3f207e1a5ee02753
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-06-15 21:40:50 -0400

    s 5 lookup

--- p3/p4/regex_engine.py
+++ p3/p4/regex_engine.py
@@ -22,7 +22,7 @@ def match_equal_length(regex, string):
     else:
         return False"""
 
-
+"""
 def match_regex_pair(input_str):
     regex, string = input_str.split('|')
     if regex.startswith('^') and regex.endswith('$'):
@@ -77,29 +77,39 @@ def match_regex(regex, string):
         return True
     else:
         return False
-
-
-# Test cases
-print(match_regex_pair('^app|apple'))  # Output: True
-print(match_regex_pair('le|apple'))  # Output: True
-print(match_regex_pair('^apple|apple'))  # Output: True
-print(match_regex_pair('apple$|apple'))  # Output: True
-print(match_regex_pair('^apple$|apple'))  # Output: True
-print(match_regex_pair('apple$|pineapple'))  # Output: False
-print(match_regex_pair('^apple|pineapple'))  # Output: False
-
 """
-print(match_regex('apple|apple'))  # Output: True
-print(match_regex('ap|apple'))  # Output: True
-print(match_regex('le|apple'))  # Output: True
-print(match_regex('a|apple'))  # Output: True
-print(match_regex('.|apple'))  # Output: True
-print(match_regex('apwle|apple'))  # Output: True
-print(match_regex('peach|apple'))  # Output: True"""
 
-# print(match_regex_pair('apple | apple'))  # Output: True
-# print(match_regex_pair('.pple | apple'))  # Output: True
-# print(match_regex_pair('appl. | apple'))  # Output: True
-# print(match_regex_pair('..... | apple'))  # Output: True
-# print(match_regex_pair('peach | apple'))  # Output: False
-# print(match_regex_pair('a | a'))  # Output:
+
+def compare(regex, string):
+    if not regex or regex == '$' and not string:
+        return True
+    if len(regex) > 1 and regex[1] in ('?', '*', '+'):
+        return quantifiers(regex, string)
+    if not string or regex[0] not in (string[0], '.'):
+        return False
+    return compare(regex[1:], string[1:])
+
+
+def quantifiers(regex, string):
+    if regex[1] in ('*', '+') and len(string) > 1 and (string[0] == string[1] == regex[0] or regex[0] == '.'):
+        return (compare(regex[2:], string) if regex[1] != '+' else compare(regex[2:], string[1:])) \
+            or compare(regex, string[1:])
+    elif string and (regex[0] in (string[0], '.')):
+        return regex[1] != '+' and compare(regex[2:], string) or compare(regex[2:], string[1:])
+    else:
+        return False if regex[1] == '+' else compare(regex[2:], string)
+
+
+def change_pos(regex, string):
+    return compare(regex, string) or bool(string) and change_pos(regex, string[1:])
+
+
+def check_caret(regex, string):
+    return compare(regex[1:], string) if regex.startswith('^') else change_pos(regex, string)
+
+
+def main():
+    data = input().split('|')
+    print(check_caret(*data))
+
+
